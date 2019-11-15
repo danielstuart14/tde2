499cb3060760997e90f7c1058234b3856d702553
Your Name <you@example.com>
2019-11-15 20:53:13 -0300

    ppm counter library

---
+++ countppm.h
@@ -0,0 +1,48 @@
+#ifndef COUNTPPM_H
+#define COUNTPPM_H
+
+#include <stdio.h>
+
+/*
+ * Estado de cada pixel da imagem.
+  * OBS: Devido o uso de calloc, o estado inicial é sempre zero.
+ */
+#define FUNDO 1
+#define LENDO 2
+#define LIDO 3
+
+/*
+ * Struct para cada pixel na imagem, contendo valores RGB
+ * e o tipo de pixel (conforme macros acima).
+ */
+struct PixelInfo {
+    int r, g, b;
+    int tipo;
+};
+
+/*
+ * Struct da imagem, contendo suas propriedades e um ponteiro 
+ * para o primeiro pixel.
+ */
+struct ImageInfo {
+    int largura, altura, intensidade;
+    struct PixelInfo *inicio;
+};
+
+/*
+ * Funções da biblioteca 
+ */
+FILE *readFile();
+
+struct PixelInfo *getPixel(struct ImageInfo *, int, int);
+
+int contarObjetos(struct ImageInfo *, int);
+int getNumber(FILE *, int *);
+int power(int, int);
+
+void abrir(struct ImageInfo *, int, int, int);
+void image2Struct (FILE *, struct PixelInfo *);
+void cor(int, int, int, int);
+void printImage(struct ImageInfo *);
+
+#endif
